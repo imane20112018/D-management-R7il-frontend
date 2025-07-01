bf6bffaa28f4bcbe4961c0185ceb6f7f7e62977f
imane <imaneeddouni2000@gmail.com>
2025-07-01 17:04:51 +0100

    add logo

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -8,8 +8,19 @@
             <!-- <img class="logo-default" srcset="/images/logo.png, /images/logo@2x.png 2x" src="/images/logo@2x.png" alt="Logo R7il">
             <img class="logo-dark" srcset="/images/logo-dark.png, /images/logo-dark@2x.png 2x" src="/images/logo-dark@2x.png" alt="Logo R7il"> -->
 
-            <img class="logo-default" :style="{ color: 'white' }" alt="Logo R7il">
-            <img class="logo-dark" :style="{ color: 'white' }" Z alt="Logo R7il">
+            <img
+              class="logo-default"
+              srcset="/template\images\33.png"
+              :style="{ color: 'white' }"
+              alt="Logo R7il"
+            />
+            <img
+              class="logo-dark"
+              srcset="/template\images\33.png"
+              :style="{ color: 'white' }"
+              Z
+              alt="Logo R7il"
+            />
           </a>
         </div>
 
@@ -19,14 +30,22 @@
             <i class="i-plain bi-telephone m-0"></i>
             <div class="he-text fw-normal text-white-50">
               Appelez-nous :
-              <span><a href="tel:+212 619 222 222" class="text-white fw-medium">+212 619 222 222</a></span>
+              <span
+                ><a href="tel:+212 619 222 222" class="text-white fw-medium"
+                  >+212 619 222 222</a
+                ></span
+              >
             </div>
           </li>
           <li>
             <i class="i-plain bi-envelope m-0"></i>
             <div class="he-text fw-normal text-white-50">
               Écrivez-nous :
-              <span><a href="mailto:R7il@R7il.ma" target="_top" class="text-white fw-medium">R7il@R7il.ma</a></span>
+              <span
+                ><a href="mailto:R7il@R7il.ma" target="_top" class="text-white fw-medium"
+                  >R7il@R7il.ma</a
+                ></span
+              >
             </div>
           </li>
         </ul>
@@ -36,17 +55,22 @@
     <div id="header-wrap">
       <div class="container">
         <div class="header-row justify-content-between flex-row-reverse flex-lg-row">
-
           <div class="header-misc">
             <!-- Recherche -->
             <div id="top-search" class="header-misc-icon">
-              <a href="#" id="top-search-trigger"><i class="uil uil-search"></i><i class="bi-x-lg"></i></a>
+              <a href="#" id="top-search-trigger"
+                ><i class="uil uil-search"></i><i class="bi-x-lg"></i
+              ></a>
             </div>
 
             <!-- Bouton d’action -->
             <div class="header-buttons d-none d-sm-inline-block">
-              <router-link to="/login_register" data-scrollto="#slider" data-offset="-80"
-                class="button button-rounded button-white button-light button-small m-0">
+              <router-link
+                to="/login_register"
+                data-scrollto="#slider"
+                data-offset="-80"
+                class="button button-rounded button-white button-light button-small m-0"
+              >
                 Se Connecter
               </router-link>
             </div>
@@ -73,28 +97,41 @@
                 </router-link>
               </li>
 
-              <li class="menu-item"><router-link class="menu-link" to="#">
+              <li class="menu-item">
+                <router-link class="menu-link" to="#">
                   <div>Tarifs</div>
                 </router-link>
                 <ul class="sub-menu-container" data-class="up-lg:not-dark">
-                  <li class="menu-item"><a class="menu-link" href="#">
+                  <li class="menu-item">
+                    <a class="menu-link" href="#">
                       <div><i class="bi-house"></i>Déménagement résidentiel</div>
-                    </a></li>
-                  <li class="menu-item"><a class="menu-link" href="#">
+                    </a>
+                  </li>
+                  <li class="menu-item">
+                    <a class="menu-link" href="#">
                       <div><i class="bi-building"></i>Déménagement de bureau</div>
-                    </a></li>
-                  <li class="menu-item"><a class="menu-link" href="#">
+                    </a>
+                  </li>
+                  <li class="menu-item">
+                    <a class="menu-link" href="#">
                       <div><i class="bi-globe"></i>Déménagement international</div>
-                    </a></li>
-                  <li class="menu-item"><a class="menu-link" href="#">
+                    </a>
+                  </li>
+                  <li class="menu-item">
+                    <a class="menu-link" href="#">
                       <div><i class="fa-solid fa-paw"></i>Transport d’animaux</div>
-                    </a></li>
-                  <li class="menu-item"><a class="menu-link" href="#">
+                    </a>
+                  </li>
+                  <li class="menu-item">
+                    <a class="menu-link" href="#">
                       <div><i class="bi-car-front"></i>Transport de voiture</div>
-                    </a></li>
-                  <li class="menu-item"><a class="menu-link" href="#">
+                    </a>
+                  </li>
+                  <li class="menu-item">
+                    <a class="menu-link" href="#">
                       <div><i class="bi-truck"></i>Louer un camion</div>
-                    </a></li>
+                    </a>
+                  </li>
                 </ul>
               </li>
               <li class="menu-item" :class="{ current: $route.path.startsWith('/transporteur') }">
@@ -103,8 +140,7 @@
                 </router-link>
               </li>
 
-
-                <li class="menu-item" :class="{ current: $route.path.startsWith('/contact') }">
+              <li class="menu-item" :class="{ current: $route.path.startsWith('/contact') }">
                 <router-link class="menu-link" to="/contact">
                   <div>Contact</div>
                 </router-link>
@@ -114,7 +150,13 @@
 
           <!-- Formulaire de recherche -->
           <form class="top-search-form" action="/recherche" method="get">
-            <input type="text" name="q" class="form-control" placeholder="Tapez votre recherche..." autocomplete="off">
+            <input
+              type="text"
+              name="q"
+              class="form-control"
+              placeholder="Tapez votre recherche..."
+              autocomplete="off"
+            />
           </form>
         </div>
       </div>
@@ -126,7 +168,7 @@
 
 <script>
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
 }
 </script>
 
