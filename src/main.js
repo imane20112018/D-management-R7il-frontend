3c4ef853c8020823a6504f66a903484709854407
imane <imaneeddouni2000@gmail.com>
2025-07-04 17:04:26 +0100

    add Dashboard

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import { createPinia } from 'pinia'
 
 // Import CSS global
 import './assets/template/css/global.css'
+import './assets/argon/css/argon-dashboard.css'
+// import './assets/argon/css/argon-dashboard.css.map'
+import './assets/argon/css/argon-dashboard.min.css'
+import './assets/argon/css/nucleo-icons.css'
+import './assets/argon/css/nucleo-svg.css'
 
 // Créer une instance de l'application Vue
 const app = createApp(App)
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 // Dashboard views
-
 import LoginView from '../views/dashboard/LoginView.vue'
 import DashboardView from '../views/dashboard/DashboardView.vue'
 import RegisterView from '../views/dashboard/RegisterView.vue'
+import ProfileView from '../views/dashboard/ProfileView.vue'
 
 // Template views
 import ContactPage from '../views/template/contact.vue'
@@ -14,19 +14,17 @@ import PresentationPage from '../views/template/presentation.vue'
 import TransporteurPage from '../views/template/transporteur.vue'
 import LoginRegisterPage from '../views/template/login_register.vue'
 
-
-
 const routes = [
   { path: '/login', component: LoginView },
   { path: '/dashboard', component: DashboardView },
+  { path: '/profile', component: ProfileView },
   { path: '/register', component: RegisterView },
   { path: '/', component: HomePage },
   { path: '/presentation', component: PresentationPage },
   { path: '/transporteur', component: TransporteurPage },
   { path: '/contact', component: ContactPage },
-  { path: '/login_register', component: LoginRegisterPage }
+  { path: '/login_register', component: LoginRegisterPage },
 ]
-
 
 const router = createRouter({
   history: createWebHistory(),
