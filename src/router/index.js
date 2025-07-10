be227b43f121b919eb147ea7707c688c45396b58
imane <imaneeddouni2000@gmail.com>
2025-07-10 17:15:22 +0100

    add register route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
 // Template views
 import ContactPage from '@/views/template/contact.vue'
 import HomePage from '@/views/template/Home.vue'
@@ -8,24 +7,21 @@ import PresentationPage from '@/views/template/presentation.vue'
 import TransporteurPage from '@/views/template/transporteur.vue'
 import Dashboard from '@/views/DashboardC/Dashboard.vue'
 import login_client from '@/views/DashboardC/Login.vue'
-
+import register_client from '@/views/DashboardC/Register.vue'
 const routes = [
-    // Public/template views
-    { path: '/', component: HomePage },
-    { path: '/presentation', component: PresentationPage },
-    { path: '/transporteur', component: TransporteurPage },
-    { path: '/contact', component: ContactPage },
-    { path: '/dashboard_client', component: Dashboard },
-    { path: '/login_client', component: login_client },
-
-
-
-
+  // Public/template views
+  { path: '/', component: HomePage },
+  { path: '/presentation', component: PresentationPage },
+  { path: '/transporteur', component: TransporteurPage },
+  { path: '/contact', component: ContactPage },
+  { path: '/dashboard_client', component: Dashboard },
+  { path: '/login_client', component: login_client },
+  { path: '/register_client', component: register_client },
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
