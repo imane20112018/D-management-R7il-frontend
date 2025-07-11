1a5b5d4f8cfdc131e5be220db3c4ccf0a033c3be
imane <imaneeddouni2000@gmail.com>
2025-07-11 17:12:04 +0100

    add routes

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,27 @@
     <router-view />
   </component>
 </template>
-
 <script>
 import MainLayout from './layout/template/MainLayout.vue'
 import DashboardLayout from '@/layout/template/DashboardLayout.vue'
-
 export default {
   computed: {
     layout() {
       const mainLayoutRoutes = ['/', '/presentation', '/transporteur', '/contact']
-
-      const dashboardLayoutRoutes = ['/dashboard_client', '/login_client', '/register_client']
-
+      const dashboardLayoutRoutes = [
+        '/dashboard_client',
+        '/login_client',
+        '/register_client',
+        '/reset_password',
+        '/logout',
+        '/forgot_password',
+      ]
       // Vérifie la route actuelle et retourne le layout approprié
       if (mainLayoutRoutes.includes(this.$route.path)) {
         return 'MainLayout'
       } else if (dashboardLayoutRoutes.includes(this.$route.path)) {
         return 'DashboardLayout'
       }
-
       // Layout par défaut si aucun ne correspond
       return 'MainLayout'
     },
