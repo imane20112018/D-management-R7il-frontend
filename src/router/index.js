df3fc17d36d055309de96b8c94dc3715192d15b0
imane <imaneeddouni2000@gmail.com>
2025-07-11 17:16:23 +0100

    import routes

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,9 @@ import TransporteurPage from '@/views/template/transporteur.vue'
 import Dashboard from '@/views/DashboardC/Dashboard.vue'
 import login_client from '@/views/DashboardC/Login.vue'
 import register_client from '@/views/DashboardC/Register.vue'
+import ResetPassword from '@/views/DashboardC/ResetPassword.vue'
+import LogoutPage from '@/views/DashboardC/Logout.vue'
+import ForgotPassword from '@/views/DashboardC/ForgotPassword.vue'
 const routes = [
   // Public/template views
   { path: '/', component: HomePage },
@@ -17,6 +20,9 @@ const routes = [
   { path: '/dashboard_client', component: Dashboard },
   { path: '/login_client', component: login_client },
   { path: '/register_client', component: register_client },
+  { path: '/reset_password', component: ResetPassword },
+  { path: '/logout', component: LogoutPage },
+  { path: '/forgot_password', component: ForgotPassword },
 ]
 
 const router = createRouter({
